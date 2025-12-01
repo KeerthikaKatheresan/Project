c5355fafd09c564fa187d5074343b0cd8e26d6e5
Keerthika Katheresan <keerthikakatheresan@gmail.com>
2025-12-01 17:58:41 +0530

    Create App.java

---
+++ Java/App.java
@@ -0,0 +1,31 @@
+package com.mphasis.TAX_CALULATION;
+
+import java.util.Scanner;
+
+public class App {
+	public static void main(String[] args) {
+		// Welcome message
+		System.out.println("+------------------------------------+");
+		System.out.println("|  WELCOME TO TAX CALCULATION APP");
+		System.out.println("+------------------------------------+");
+		System.out.println("Please Login to Continue");
+
+		// Asking user for login credentials
+		Scanner sc = new Scanner(System.in);
+		System.out.println("USERNAME:");
+		String username = sc.nextLine();
+		System.out.println("PASSWORD:");
+		String password = sc.nextLine();
+
+		// Connecting to Database
+		DBConnection db = new DBConnection();
+		if (db.login(username, password)) {
+			System.out.println("Login Successful");
+			TaskMenu menu = new TaskMenu();
+			menu.showMenu();
+		} else {
+			System.out.println("Login failed.Username or Password invalid");
+		}
+
+	}
+}
