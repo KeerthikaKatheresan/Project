a6922f1085a66c3d4891e5f4048f7171673dc031
Keerthika Katheresan <keerthikakatheresan@gmail.com>
2025-12-02 18:07:43 +0530

    Create TotalTax.java

---
+++ Java/TotalTax.java
@@ -0,0 +1,70 @@
+package com.mphasis.TAX_CALULATION;
+
+public class TotalTax {
+
+	private int propertyQuantity;
+	private int vehicleQuantity;
+	private int totalPropertyTax;
+	private int totalVehicleTax;
+	private int totalTax;
+	/**
+	 * 
+	 */
+	public TotalTax() {
+		super();
+	}
+	/**
+	 * @param propertyQuantity
+	 * @param vehicleQuantity
+	 * @param totalPropertyTax
+	 * @param totalVehicleTax
+	 * @param totalTax
+	 */
+	public TotalTax(int propertyQuantity, int vehicleQuantity, int totalPropertyTax, int totalVehicleTax,
+			int totalTax) {
+		super();
+		this.propertyQuantity = propertyQuantity;
+		this.vehicleQuantity = vehicleQuantity;
+		this.totalPropertyTax = totalPropertyTax;
+		this.totalVehicleTax = totalVehicleTax;
+		this.totalTax = totalTax;
+	}
+	public int getPropertyQuantity() {
+		return propertyQuantity;
+	}
+	public void setPropertyQuantity(int propertyQuantity) {
+		this.propertyQuantity = propertyQuantity;
+	}
+	public int getVehicleQuantity() {
+		return vehicleQuantity;
+	}
+	public void setVehicleQuantity(int vehicleQuantity) {
+		this.vehicleQuantity = vehicleQuantity;
+	}
+	public int getTotalPropertyTax() {
+		return totalPropertyTax;
+	}
+	public void setTotalPropertyTax(int totalPropertyTax) {
+		this.totalPropertyTax = totalPropertyTax;
+	}
+	public int getTotalVehicleTax() {
+		return totalVehicleTax;
+	}
+	public void setTotalVehicleTax(int totalVehicleTax) {
+		this.totalVehicleTax = totalVehicleTax;
+	}
+	public int getTotalTax() {
+		return totalTax;
+	}
+	public void setTotalTax(int totalTax) {
+		this.totalTax = totalTax;
+	}
+	@Override
+	public String toString() {
+		return "TotalTax [propertyQuantity=" + propertyQuantity + ", vehicleQuantity=" + vehicleQuantity
+				+ ", totalPropertyTax=" + totalPropertyTax + ", totalVehicleTax=" + totalVehicleTax + ", totalTax="
+				+ totalTax + "]";
+	}
+	
+	
+}
