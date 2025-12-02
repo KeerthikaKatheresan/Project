5a84b90c27c6083f8c4337eabdf75a0c64919411
Keerthika Katheresan <keerthikakatheresan@gmail.com>
2025-12-02 18:06:38 +0530

    Update Property.java

--- Java/Property.java
+++ Java/Property.java
@@ -1,10 +1,22 @@
 package com.mphasis.TAX_CALULATION;
 
 public class Property {
+	private static int counter = 0;
+	private int Id;
 	private int baseValue;
 	private int buildUpArea;
 	private int landAge;
 	private char landLocation;
+	private int propertyTax;
+
+	
+	/**
+	 * 
+	 */
+	public Property() {
+		super();
+	}
+
 	/**
 	 * @param baseValue
 	 * @param buildUpArea
@@ -13,28 +25,46 @@ public class Property {
 	 */
 	public Property(int baseValue, int buildUpArea, int landAge, char landLocation) {
 		super();
+		this.Id = ++counter;
 		this.baseValue = baseValue;
 		this.buildUpArea = buildUpArea;
 		this.landAge = landAge;
 		this.landLocation = landLocation;
+		this.propertyTax = 0;
 	}
+
+	public int getId() {
+		return Id;
+	}
+
 	public int getBaseValue() {
 		return baseValue;
 	}
+
 	public int getBuildUpArea() {
 		return buildUpArea;
 	}
+
 	public int getLandAge() {
 		return landAge;
 	}
+
 	public char getLandLocation() {
 		return landLocation;
 	}
+
+	public int getPropertyTax() {
+		return propertyTax;
+	}
+
+	public void setPropertyTax(int propertyTax) {
+		this.propertyTax = propertyTax;
+	}
+
 	@Override
 	public String toString() {
-		return "Property [baseValue=" + baseValue + ", buildUpArea=" + buildUpArea + ", landAge=" + landAge
-				+ ", landLocation=" + landLocation + "]";
+		return "Property [Id=" + Id + ", baseValue=" + baseValue + ", buildUpArea=" + buildUpArea + ", landAge="
+				+ landAge + ", landLocation=" + landLocation + ", propertyTax=" + propertyTax + "]";
 	}
-	
 
 }
